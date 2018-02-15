61650b92c5fb04ae8d4f8a91d00909e7d3ab6f0a
Rafael Levy <crlevys@itleon.edu.mx>
2018-02-15 12:15:12 -0600

    Anadir funcion flash y utilizar el semaforo en modo MX

--- InteractiveTrafficLights.ino
+++ InteractiveTrafficLights.ino
@@ -2,7 +2,7 @@
  *   Interactive Traffic Lights
  *   
  *   Este ejemplo muestra un conjunto de semáforos que cambiarán de verde a rojo, ámbar y viceversa, 
- *   luego de un período de tiempo establecido, utilizando el sistema de cuatro estados del Reino Unido.
+ *   luego de un período de tiempo establecido, utilizando el sistema de cuatro estados de Mexico.
  *   Este se extiende para incluir un conjunto de luces peatonales y un botón peatonal para solicitar 
  *   cruzar la calle. El Arduino reaccionará cuando se presione el botón cambiando el estado de las 
  *   luces para que los autos se detengan y permitan que el peatón cruce con seguridad.
@@ -46,7 +46,7 @@ void loop() {
  * Rutina que realiza el cambio de color en los semaforos cuando el boton es presionado
  */
 void changeLights() {
-  digitalWrite(CAR_GREEN, LOW); // green off
+  flash(CAR_GREEN); // flash green
   digitalWrite(CAR_YELLOW, HIGH); // yellow on
   delay(2000); // wait 2 seconds
   digitalWrite(CAR_YELLOW, LOW); // yellow off
@@ -55,13 +55,7 @@ void changeLights() {
   digitalWrite(PED_RED, LOW); // ped red off
   digitalWrite(PED_GREEN, HIGH); // ped green on
   delay(crossTime); // wait for preset time period
-  // flash the ped green
-  for (int x=0; x<10; x++) {
-    digitalWrite(PED_GREEN, HIGH);
-    delay(250);
-    digitalWrite(PED_GREEN, LOW);
-    delay(250);
-  }
+  flash(PED_GREEN); // flash the ped green
   // turn ped red on
   digitalWrite(PED_RED, HIGH);
   delay(500);
@@ -74,4 +68,16 @@ void changeLights() {
   // record the time since last change of lights
   changeTime = millis();
   // then return to the main program loop
+}
+
+
+// flash the light in pin n
+void flash(int pin){
+  
+  for (int x=0; x<6; x++) {
+    digitalWrite(pin, HIGH);
+    delay(250);
+    digitalWrite(pin, LOW);
+    delay(250);
+  }
 }
